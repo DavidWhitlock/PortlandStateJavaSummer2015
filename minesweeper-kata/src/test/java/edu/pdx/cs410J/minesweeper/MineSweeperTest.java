e1d6c7f0404597acf19c6a1a92232573831369f8
David Whitlock <david.m.whitlock@gmail.com>
2015-07-22 19:41:41 -0700

    Captured a bunch of test cases.

--- minesweeper-kata/src/test/java/edu/pdx/cs410J/minesweeper/MineSweeperTest.java
+++ minesweeper-kata/src/test/java/edu/pdx/cs410J/minesweeper/MineSweeperTest.java
@@ -11,6 +11,20 @@ import static org.junit.Assert.assertTrue;
  */
 public class MineSweeperTest extends InvokeMainTestCase {
 
+  // What should we test?
+  // Is there a matrix?
+  // Zero by zero matrix
+  // Zero by non-zero matrix
+  // Negative matrix size
+  // Non-zero by non-zero matrix
+  // Make sure that the matrix specified by mine field matches dimensions
+  // Making sure that the number of ajancent mines is correct
+  // No mines
+  // All mines
+  // Are mines correctly identified in output?
+  // Bad character (not * and not .) in input
+  // Make sure input has newlines
+
     /**
      * Invokes the main method of {@link Minesweeper} with the given arguments.
      */
