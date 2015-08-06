16a27b3d982f8d4482e5214c1085f7ef903ae690
David Whitlock <david.m.whitlock@gmail.com>
2015-08-05 18:45:10 -0700

    Add the start screen to a DockPanel.

--- minesweeper-gwt/src/main/java/edu/pdx/cs410J/minesweeper/client/PhoneBillGwt.java
+++ minesweeper-gwt/src/main/java/edu/pdx/cs410J/minesweeper/client/PhoneBillGwt.java
@@ -7,17 +7,28 @@ import com.google.gwt.user.client.ui.*;
  * A basic GWT class that makes sure that we can send an Phone Bill back from the server
  */
 public class PhoneBillGwt implements EntryPoint {
+  @Override
   public void onModuleLoad() {
 
+
+    VerticalPanel startScreen = createStartScreen();
+
+    DeckPanel gamePanels = new DeckPanel();
+    gamePanels.add(startScreen);
+
+    RootPanel rootPanel = RootPanel.get();
+    rootPanel.add(gamePanels);
+    gamePanels.showWidget(0);
+  }
+
+  private VerticalPanel createStartScreen() {
     VerticalPanel startScreen = new VerticalPanel();
     startScreen.add(new Label("Enter game board size"));
 
     startScreen.add(createRowsPanel());
     startScreen.add(createColumnsPanel());
     startScreen.add(new Button("Create Game"));
-
-    RootPanel rootPanel = RootPanel.get();
-    rootPanel.add(startScreen);
+    return startScreen;
   }
 
   private HorizontalPanel createRowsPanel() {
